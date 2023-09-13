7774a8c264fc1208a3dcb3b375829f147263d05c
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-09-13 19:01:56 -0400

    Add Expenses List

--- code/01-using-stateful-lists/src/components/NewExpense/NewExpense.js
+++ code/01-using-stateful-lists/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setShowForm(false);
   };
 
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const cancelHandler = () => {
+    setShowForm(false);
+  }
+
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {showForm ? (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancelHandler={cancelHandler}/>
+      ) : (
+        <button onClick={showFormHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
